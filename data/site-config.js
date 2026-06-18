de48c01b008c93ca884dc0c4a491c296268f6e51
kimjiyeun70-svg <kimjiyeun70@gmail.com>
2026-06-18 09:19:50 +0900

    AutoPilot: update data/site-config.js

---
+++ data/site-config.js
@@ -0,0 +1,7 @@
+window.siteConfig = {
+  "siteName": "Class Action Tracker",
+  "primaryColor": "#7c2d12",
+  "bgMain": "#fff7ed",
+  "fontHeading": "'Merriweather', serif",
+  "layoutStyle": "serif"
+};
